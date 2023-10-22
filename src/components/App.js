11332ca228838e2039882aed72985c5047d0391d
NIHAR-1996 <niharkumarsahoo@gmail.com>
2023-10-22 12:17:43 +0530

    submitting code-Sun Oct 22 2023 12:17:38 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,48 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const {todo,setTodo}=useState("");
+  const {todoList,setTodoList}=(["Task 1","Task 2"]);
+
+  function applyAddTodo(e){
+    e.preventDefault()
+    setTodoList([...todoList,todo])
+    setTodo("")
+  }
+
+  function handleDelete(index) {
+    console.log("item to be deleted" ,index)
+    setTodoList(todoList.filter((value,position)=>position!==index))
+  }
+  
+  
+  
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+   <div>
+     <form onSubmit={applyAddTodo}>
+       <input type="text" placeholder="Enter Task"
+       onChange={(e) => setTodo(e.target.value)}
+       value={todo}
+       />
+       <button type="submit">Add Todo</button>
+     </form>
+
+     {
+      todoList.map((item , index) => (
+        
+        <ul>
+          <li>
+            {item}
+            <button onClick={()=>handleDelete(index)}>Delete</button>
+          </li>
+        </ul>
+        
+      ))
+     }
+   </div>
+   
   )
 }
 
